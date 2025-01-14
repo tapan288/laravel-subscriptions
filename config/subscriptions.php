bd615e00f733112f56f01116e9096c493787156a
Tapan Sharma <tapansharma69@gmail.com>
2025-01-14 22:06:40 +0545

    creating and storing plans

---
+++ config/subscriptions.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'plans' => [
+        'monthly' => [
+            'name' => 'Monthly',
+            'price_id' => 'price_1QhCn7SJY7yVeuxPXe9O3RI1',
+        ],
+        'yearly' => [
+            'name' => 'Yearly',
+            'price_id' => 'price_1QhCnXSJY7yVeuxPitbyXoil',
+        ],
+    ],
+];
